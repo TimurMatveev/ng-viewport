603d841650be99f087efca162e3a8a44f26a2eaa
Timur Matveev <tmatveev@transperfect.com>
2025-01-03 17:21:46 +0300

    init

--- src/app/lib/ng-viewport/components/ng-viewport/ng-viewport.component.ts
+++ src/app/lib/ng-viewport/components/ng-viewport/ng-viewport.component.ts
@@ -12,8 +12,36 @@ export class NgViewportComponent {
 
   maxTouches = window.navigator.maxTouchPoints;
 
+  constructor() {
+    // setInterval(() => {
+    //   this.touches.set([
+    //     { id: 1, x: Math.random(), y: Math.random() },
+    //     { id: 2, x: Math.random(), y: Math.random() },
+    //     { id: 3, x: Math.random(), y: Math.random() },
+    //   ])
+    // }, 1000);
+  }
+
   onTouchStart(event: TouchEvent): void {
+    if (!this.maxTouches) {
+      return;
+    }
+
     event.preventDefault();
     this.touches.set(Array.from(event.touches).map((touch, index) => ({ id: index + 1, x: touch.screenX, y: touch.screenY })));
+  }
+
+  onMouseDown(event: MouseEvent): void {
+    if (this.maxTouches) {
+      return;
+    }
+
+    event.preventDefault();
+    this.touches.set([{ id: 1, x: event.screenX, y: event.screenY }]);
+  }
+
+  counter = 1;
+  increment(): void {
+    this.counter++;
   }
 }
